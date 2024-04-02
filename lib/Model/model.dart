e89b24b3f772a78cf3263c2422f9911c90be99ce
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-04-02 20:00:44 +0100

    added

--- lib/Model/model.dart
+++ lib/Model/model.dart
@@ -1,2 +1,43 @@
 
+class Breed {
+  final String name;
+  final BreedImage? image;
+
+  Breed({
+    required this.name,
+    this.image,
+  });
+
+  factory Breed.fromJson(Map<String, dynamic> json) {
+    return Breed(
+      name: json['name'],
+      image: json['image'] != null ? BreedImage.fromJson(json['image']) : null,
+    );
+  }
+}
+
+class BreedImage {
+  final String? url;
+  final int? height;
+  final String? id;
+  final int? width;
+
+  BreedImage({
+    this.url,
+    this.height,
+    this.id,
+    this.width,
+  });
+
+  factory BreedImage.fromJson(Map<String, dynamic> json) {
+    return BreedImage(
+      url: json['url'],
+      height: json['height'],
+      id: json['id'],
+      width: json['width'],
+    );
+  }
+}
+
+typedef CatBreed = List<Breed>;
 
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,35 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
-class Gridhomepage extends StatelessWidget {
+class Gridhomepage extends StatefulWidget {
   const Gridhomepage({Key? key}) : super(key: key);
 
   @override
+  State<Gridhomepage> createState() => _GridhomepageState();
+}
+
+class _GridhomepageState extends State<Gridhomepage> {
+  List<String> catImages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCatImages().then((images) {
+      setState(() {
+        catImages = images;
+      });
+    });
+  }
+
+  Future<List<String>> fetchCatImages() async {
+    final String apiUrl = 'https://api.thecatapi.com/v1/breeds';
+
+    try {
+      Dio dio = Dio();
+      Response response = await dio.get(apiUrl);
+      if (response.statusCode == 200) {
+        List<dynamic> breedsData = response.data;
+        List<String> imageUrls = breedsData
+            .map((breed) => breed['image'] != null ? breed['image']['url'] : null)
+            .where((url) => url != null)
+            .cast<String>()
+            .toList();
+        return imageUrls;
+      } else {
+        print('Failed to fetch cat images. Status code: ${response.statusCode}');
+        return [];
+      }
+    } catch (error) {
+      print('Error: $error');
+      return [];
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(appBar: AppBar(
-        title: Text("Pix Gallerie"),
-    ) ,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Pix Gallerie"),
+        ),
         body: Container(
           color: Colors.grey,
-          child: GridView.count(crossAxisCount: 3,
+          child: GridView.count(
+            crossAxisCount: 3,
             crossAxisSpacing: 10.0,
             mainAxisSpacing: 10.0,
             padding: EdgeInsets.all(10.0),
-            children:
-                List.generate(100, (index) {
-                  return Container(
-                    color: Colors.black12,
-                    child: Center(
-                     child: Text('chi',
-                     style: TextStyle(fontSize: 16),
-                     ),
-                    ),
-                  );
-                }
-                )
+            children: List.generate(catImages.length, (index) {
+              return CachedNetworkImage(
+                imageUrl: catImages[index],
+                fit: BoxFit.cover,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              );
+            }),
           ),
         ),
-      )
+      ),
     );
   }
 }
